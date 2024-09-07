b9aa73337efb1501bd5cff4cfd451bcb769e652c
Musab Bahadır Bayram <musab.bayram@hotmail.com>
2024-09-07 18:09:19 +0300

    Update Router.js

--- Router.js
+++ Router.js
@@ -1,30 +1,33 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import LoginScreen from './screens/LoginScreen';
-import HomeScreen from './screens/HomeScreen';
+import SignUpScreen from './screens/SignUpScreen';
+import HomeScreen from './screens/HomeScreen'; // Ana ekran (dilersen)
 
 const Stack = createNativeStackNavigator();
 
 const Router = () => {
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
         <Stack.Screen
           name="Login"
           component={LoginScreen}
-          options={{ headerShown: false }} // Header gizleme
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="SignUp"
+          component={SignUpScreen}
+          options={{ headerShown: false }}
         />
         <Stack.Screen
           name="Home"
           component={HomeScreen}
-          options={{ headerShown: false }} // Header gizleme
+          options={{ headerShown: false }}
         />
       </Stack.Navigator>
     </NavigationContainer>
-    </GestureHandlerRootView>
   );
 };
 
