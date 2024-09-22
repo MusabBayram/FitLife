181c515c88905bcb344693e92f27b261ceacd908
Musab Bahadır Bayram <musab.bayram@hotmail.com>
2024-09-22 23:12:44 +0300

    Update Router.js

--- Router.js
+++ Router.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LoginScreen from './screens/LoginScreen';
-import SignUpScreen from './screens/SignUpScreen';
+import LoginScreen from './src/screens/LoginScreen';
+import MainTabView from './src/screens/MainTabView';
 
 const Stack = createNativeStackNavigator();
 
@@ -10,8 +10,16 @@ const Router = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="SignUp" component={SignUpScreen} options={{ headerShown: false }} />
+        <Stack.Screen n
+          name="Login" 
+          component={LoginScreen} 
+          options={{ headerShown: false }} 
+        />
+        <Stack.Screen
+          name="Main"
+          component={MainTabView}
+          options={{ headerShown: false }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
